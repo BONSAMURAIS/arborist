309b1a8307dbd8b32cff8aca7b736ebf9d455774
Tiago Morais <42031245+tgmorais1@users.noreply.github.com>
2019-03-28 17:39:55 +0000

    Update __init__.py

--- arborist/__init__.py
+++ arborist/__init__.py
@@ -11,3 +11,4 @@ data_dir = Path(__file__).parent / "data"
 
 from .climate_change import generate_climate_change_uris
 from .time_uris import generate_time_uris
+from .us_epa import generate_us_epa_uris
