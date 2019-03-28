7a4401b87fdd52d8f2504d234749bbce9ed89416
Tiago Morais <42031245+tgmorais1@users.noreply.github.com>
2019-03-28 18:27:55 +0000

    Update __init__.py

--- arborist/__init__.py
+++ arborist/__init__.py
@@ -3,6 +3,7 @@ __all__ = (
     "generate_climate_change_uris",
     "generate_us_epa_uris",
     "generate_exiobase_us_epa_uris",
+    "generate_electricity_mix_uris",
 )
 __version__ = (0, 1)
 
@@ -15,3 +16,4 @@ from .climate_change import generate_climate_change_uris
 from .time_uris import generate_time_uris
 from .us_epa import generate_us_epa_uris
 from .exiobase_us_epa import generate_exiobase_us_epa_uris
+from .electricity_grid import generate_electricity_mix_uris
