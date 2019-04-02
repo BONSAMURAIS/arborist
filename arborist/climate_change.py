a6504db033b72c4a98c69514826462ba96b228c0
Chris Mutel <cmutel@gmail.com>
2019-04-02 10:42:36 +0200

    Add common function to generate generic graphs

--- arborist/climate_change.py
+++ arborist/climate_change.py
@@ -2,68 +2,35 @@
 This code creates both `activityTypes` and `flowObjects`.
 
 As these are not present in any online data, we have hard coded our own URIs"""
-from . import data_dir
-from rdflib import Literal, RDF, URIRef, Namespace, Graph
-from rdflib.namespace import DC, RDFS, OWL, FOAF, XSD
-from .graph_common import NS, add_common_elements
-from .filesystem import create_dir
-from pathlib import Path
+from .graph_common import generate_generic_graph
 
 
 def generate_climate_change_uris(output_base_dir):
-    output_base_dir = Path(output_base_dir)
-    output_dir = create_dir(output_base_dir / "activitytype" / "lcia" / "climate_change")
-
-    g = add_common_elements(
-        graph=Graph(),
-        base_uri="http://rdf.bonsai.uno/activitytype/lcia/climate_change/",
+    generate_generic_graph(
+        output_base_dir,
+        kind='ActivityType',
+        data=[
+            ("Atmospheric energy balance",),
+            ("Temperature increase",),
+        ],
+        directory_structure=["lcia", "climate_change"],
         title="Climate change activity types",
         description='ActivityType instances needed for BONSAI climate change modelling',
         author="Tiago Morais",
         version="0.3",
     )
 
-    g.bind('brdfat', 'http://rdf.bonsai.uno/activitytype/lcia/climate_change/')
-
-    DATA = [
-        ("Atmospheric energy balance",
-         "http://rdf.bonsai.uno/activitytype/lcia/climate_change/atmospheric_energy_balance"),
-        ("Temperature increase",
-         "http://rdf.bonsai.uno/activitytype/lcia/climate_change/temperature_increse"),
-    ]
-
-    for label, uri in DATA:
-        node = URIRef(uri)
-        g.add( (node, RDF.type, NS.nb.ActivityType) )
-        g.add( (node, RDFS.label, Literal(label)) )
-
-    with open(output_dir / "climate_change.ttl", "wb") as f:
-        g.serialize(f, format="turtle", encoding='utf-8')
-
-    output_dir = create_dir(output_base_dir / "flowobject" / "lcia" / "climate_change")
-
-    g = add_common_elements(
-        graph=Graph(),
-        base_uri="http://rdf.bonsai.uno/flowobject/lcia/climate_change/",
+    generate_generic_graph(
+        output_base_dir,
+        kind='FlowObject',
+        data=[
+            ("Radiative forcing",),
+            ("Temperature increase - 100 year horizon",
+             "temperature_increase_100yr_horizon"),
+        ],
+        directory_structure=["lcia", "climate_change"],
         title="Climate change activity types",
         description='FlowObject instances needed for BONSAI climate change modelling',
         author="Tiago Morais",
         version="0.3",
     )
-
-    g.bind('brdffo', 'http://rdf.bonsai.uno/flowobject/lcia/climate_change/')
-
-    DATA = [
-        ("Radiative forcing",
-         "http://rdf.bonsai.uno/flowobject/lcia/climate_change/radiative_forcing"),
-        ("Temperature increase - 100 year horizon",
-         "http://rdf.bonsai.uno/flowobject/lcia/climate_change/temperature_increase_100yr_horizon"),
-    ]
-
-    for label, uri in DATA:
-        node = URIRef(uri)
-        g.add( (node, RDF.type, NS.nb.FlowObject) )
-        g.add( (node, RDFS.label, Literal(label)) )
-
-    with open(output_dir / "climate_change.ttl", "wb") as f:
-        g.serialize(f, format="turtle", encoding='utf-8')
--- arborist/electricity_grid.py
+++ arborist/electricity_grid.py
@@ -1,61 +1,31 @@
 """Generate the URIs needed for core activity type `electricity grid`.
 Handcrafted as not available elsewhere."""
-
-from . import data_dir
-from .filesystem import create_dir
-from .graph_common import NS, add_common_elements
-from pathlib import Path
-from rdflib import Graph, Literal, RDF, URIRef, Namespace
-from rdflib.namespace import RDFS
-import pandas
+from .graph_common import generate_generic_graph
 
 
 def generate_electricity_grid_uris(output_base_dir):
-    output_base_dir = Path(output_base_dir)
-
-    g = add_common_elements(
-        graph=Graph(),
-        base_uri="http://rdf.bonsai.uno/activitytype/core/electricity_grid/",
+    generate_generic_graph(
+        output_base_dir,
+        kind='ActivityType',
+        data=[
+            ("Electricity grid", "eg"),
+        ],
+        directory_structure=["core", "electricity_grid"],
         title="Electricity grid activity types for BONSAI",
         description='ActivityType instances needed for BONSAI electricity grid modelling',
         author="Arthur Jakobs",
         version="0.2",
     )
 
-    g.bind('brdfat', 'http://rdf.bonsai.uno/activitytype/core/electricity_grid/')
-
-    node = URIRef("http://rdf.bonsai.uno/activitytype/core/eg")
-    g.add( (node, RDF.type, NS.nb.ActivityType) )
-    g.add( (node, RDFS.label, Literal("Electricity grid")) )
-
-    node = URIRef("http://rdf.bonsai.uno/activitytype/core/em")
-    g.add( (node, RDF.type, NS.nb.ActivityType) )
-    g.add( (node, RDFS.label, Literal("Market for electricity")) )
-
-    create_dir(output_base_dir / "activitytype" / "core" / "electricity_grid")
-    with open(
-            output_base_dir / "activitytype" / "core" / "electricity_grid" / "electricity_grid.ttl",
-            "wb") as f:
-        g.serialize(f, format="turtle", encoding='utf-8')
-
-    g = add_common_elements(
-        graph=Graph(),
-        base_uri="http://rdf.bonsai.uno/flowobject/core/electricity_grid/",
+    generate_generic_graph(
+        output_base_dir,
+        kind='FlowObject',
+        data=[
+            ("Market for electricity", "elec"),
+        ],
+        directory_structure=["core", "electricity_grid"],
         title="Electricity grid flow objects for BONSAI",
         description='FlowObject instances needed for BONSAI electricity grid modelling',
         author="Arthur Jakobs",
         version="0.2",
     )
-
-    g.bind('brdfat', 'http://rdf.bonsai.uno/activitytype/core/electricity_grid/')
-
-    node = URIRef("http://rdf.bonsai.uno/flowobject/core/elec")
-    g.add( (node, RDF.type, NS.nb.FlowObject) )
-    g.add( (node, RDFS.label, Literal("Electricity from the grid")) )
-
-    create_dir(output_base_dir / "flowobject" / "core" / "electricity_grid")
-    with open(
-            output_base_dir / "flowobject" / "core" / "electricity_grid" / "electricity_grid.ttl",
-            "wb") as f:
-        g.serialize(f, format="turtle", encoding='utf-8')
-
