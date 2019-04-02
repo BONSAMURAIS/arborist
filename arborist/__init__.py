9ba598d8f19d24e59a834513fc0035e611db6f83
Chris Mutel <cmutel@gmail.com>
2019-04-02 16:36:07 +0200

    Convert exiobase metadata script

--- arborist/__init__.py
+++ arborist/__init__.py
@@ -2,10 +2,12 @@ __all__ = (
     "generate_climate_change_uris",
     "generate_electricity_grid_uris",
     "generate_exiobase_us_epa_uris",
+    "generate_exiobase_metadata_uris",
     "generate_foaf_uris",
     "generate_time_uris",
     "generate_unit_uris",
     "generate_us_epa_uris",
+    "generate_all",
     "get_metadata",
 )
 __version__ = (0, 1)
@@ -16,8 +18,15 @@ data_dir = Path(__file__).parent / "data"
 from .climate_change import generate_climate_change_uris
 from .electricity_grid import generate_electricity_grid_uris
 from .exiobase_us_epa import generate_exiobase_us_epa_uris
+from .exiobase_metadata import generate_exiobase_metadata_uris
 from .extract_metadata import get_metadata
 from .foaf import generate_foaf_uris
 from .time_uris import generate_time_uris
 from .units import generate_unit_uris
 from .us_epa import generate_us_epa_uris
+
+
+def generate_all(base_dir):
+    generate_climate_change_uris(base_dir)
+    generate_electricity_grid_uris(base_dir),
+    generate_exiobase_metadata_uris(base_dir),
--- arborist/exiobase_metadata.py
+++ arborist/exiobase_metadata.py
@@ -1,6 +1,6 @@
 from . import data_dir
-from .filesystem import create_dir
-from .graph_common import NS, add_common_elements
+from .filesystem import create_dir, write_graph
+from .graph_common import NS, add_common_elements, generate_generic_graph
 from pathlib import Path
 from rdflib import Graph, Literal, RDF, URIRef, Namespace
 from rdflib.namespace import DC, RDFS
@@ -15,62 +15,35 @@ def generate_exiobase_metadata_uris(output_base_dir):
         sheet_name="Activities",
         header=0
     )
-    data = set(zip(df['Activity name'], df['Activity code 2']))
 
-    nat = Namespace("http://rdf.bonsai.uno/activitytype/exiobase3_3_17/")
-
-    g = add_common_elements(
-        graph=Graph(),
-        base_uri="http://rdf.bonsai.uno/activitytype/exiobase3_3_17/",
+    generate_generic_graph(
+        output_base_dir,
+        kind='ActivityType',
+        data=sorted(set(zip(df['Activity name'], df['Activity code 2']))),
+        directory_structure=["exiobase3_3_17"],
         title="EXIOBASE 3.3.17 activity types",
         description='ActivityType instances needed for BONSAI modelling of EXIOBASE version 3.3.17',
         author="BONSAI team",
-        version="0.2",
+        version="0.3",
     )
-    g.bind('brdfat', 'http://rdf.bonsai.uno/activitytype/exiobase3_3_17/')
-
-    for name, code in data:
-        node = URIRef(nat[code])
-        g.add( (node, RDF.type, nb.ActivityType) )
-        g.add( (node, RDFS.label, Literal(name)) )
-
-    create_dir(output_base_dir / "activitytype" / "exiobase3_3_17")
-    with open(
-            output_base_dir / "activitytype" / "exiobase3_3_17" / "exiobase3_3_17.ttl",
-            "wb") as f:
-        g.serialize(f, format="turtle", encoding='utf-8')
 
     # # TODO: Need to add preferred unit
-
     df = pandas.read_excel(
         data_dir / "exiobase_classifications_v_3_3_17.xlsx",
         sheet_name="Products_HSUTs",
         header=0
     )
-    data = set(zip(df['Product name'], df['product code 2']))
 
-    nfo = Namespace("http://rdf.bonsai.uno/flowobject/exiobase3_3_17/")
-
-    g = add_common_elements(
-        graph=Graph(),
-        base_uri="http://rdf.bonsai.uno/flowobject/exiobase3_3_17/",
+    generate_generic_graph(
+        output_base_dir,
+        kind='FlowObject',
+        data=sorted(set(zip(df['Product name'], df['product code 2']))),
+        directory_structure=["exiobase3_3_17"],
         title="EXIOBASE 3.3.17 flow objects",
         description='FlowObject instances needed for BONSAI modelling of EXIOBASE version 3.3.17',
         author="BONSAI team",
-        version="0.2",
+        version="0.3",
     )
-    g.bind('brdffo', "http://rdf.bonsai.uno/flowobject/exiobase3_3_17/")
-
-    for name, code in data:
-        node = URIRef(nfo[code])
-        g.add( (node, RDF.type, nb.FlowObject) )
-        g.add( (node, RDFS.label, Literal(name)) )
-
-    create_dir(output_base_dir / "flowobject" / "exiobase3_3_17")
-    with open(
-            output_base_dir / "flowobject" / "exiobase3_3_17" / "exiobase3_3_17.ttl",
-            "wb") as f:
-        g.serialize(f, format="turtle", encoding='utf-8')
 
     # Exiobase locations are hardcoded to geoname URIs, so just use CSV
     # created by hand
@@ -80,20 +53,21 @@ def generate_exiobase_metadata_uris(output_base_dir):
     )
     data = set(zip(df['CountryCode'], df['URI']))
 
-    g = Graph()
-    g.bind('bont', 'http://ontology.bonsai.uno/core#')
-    g.bind('gn', 'http://sws.geonames.org/3202326')
+    g = add_common_elements(
+        Graph(),
+        "http://rdf.bonsai.uno/location/exiobase3_3_17/",
+        "Custom locations for EXIOBASE 3.3",
+        "Country groupings used EXIOBASE 3.3.17",
+        "Chris Mutel",
+        "0.2",
+    )
+    g.bind('gn', 'http://sws.geonames.org/')
     g.bind('brdflo', "http://rdf.bonsai.uno/location/exiobase3_3_17/")
-    g.bind("dc", DC)
     g.bind("schema", "http://schema.org/")
 
     for name, code in data:
         node = URIRef("http://" + code)
-        g.add( (node, RDF.type, URIRef("http://schema.org/Place")) )
-        g.add( (node, RDFS.label, Literal(name)) )
+        g.add((node, RDF.type, URIRef("http://schema.org/Place")))
+        g.add((node, RDFS.label, Literal(name)))
 
-    create_dir(output_base_dir / "location" / "exiobase3_3_17")
-    with open(
-            output_base_dir / "location" / "exiobase3_3_17" / "exiobase3_3_17.ttl",
-            "wb") as f:
-        g.serialize(f, format="turtle", encoding='utf-8')
+    write_graph(output_base_dir / "location" / "exiobase3_3_17", g)
