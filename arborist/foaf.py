3bd63283782d76908de2ff9a186f11f28762b96c
Chris Mutel <cmutel@gmail.com>
2019-11-06 10:20:47 +0100

    Add FOAF for Bonsai dataset. Fixes #3.

--- arborist/foaf.py
+++ arborist/foaf.py
@@ -1,7 +1,7 @@
 from .filesystem import write_graph
 from pathlib import Path
 from rdflib import Graph, Literal, RDF, URIRef, Namespace
-from rdflib.namespace import FOAF, SKOS
+from rdflib.namespace import FOAF, SKOS, DC, OWL, XSD
 
 
 def generate_foaf_uris(output_base_dir):
@@ -15,8 +15,10 @@ def generate_foaf_uris(output_base_dir):
     g.bind("org", "https://www.w3.org/TR/vocab-org/")
     g.bind("skos", SKOS)
     g.bind("foaf", FOAF)
+    g.bind("dc", DC)
+    g.bind("owl", OWL)
 
-    node = URIRef("http://bonsai.uno/foaf/foaf.ttl#bonsai")
+    node = URIRef("https://bonsai.uno/foaf/foaf.ttl#bonsai")
     g.add((node, RDF.type, org.Organization))
     g.add(
         (
@@ -27,9 +29,20 @@ def generate_foaf_uris(output_base_dir):
             ),
         )
     )
-    g.add((node, FOAF.homepage, URIRef("http://bonsai.uno")))
+    g.add((node, FOAF.homepage, URIRef("https://bonsai.uno")))
     g.add((node, FOAF.interest, URIRef("https://www.wikidata.org/wiki/Q131201")))
     g.add((node, FOAF.interest, URIRef("https://www.wikidata.org/wiki/Q2323664")))
     g.add((node, FOAF.interest, URIRef("https://www.wikidata.org/wiki/Q18692990")))
+
+    ds = URIRef("https://bonsai.uno/foaf")
+    g.add((ds, RDF.type, URIRef("https://purl.org/dc/dcmitype/Dataset")))
+    g.add((ds, DC.creator, node))
+    g.add((ds, DC.publisher, Literal("bonsai.uno")))
+    g.add((ds, DC.title, Literal("The BONSAI Organization")))
+    g.add((ds, DC.description, Literal("Contains information about the BONSAI organization")))
+    g.add((ds, OWL.versionInfo, Literal("0.2")))
+    g.add((ds, FOAF.homepage, URIRef("https://rdf.bonsai.uno/unit/documentation.html")))
+    g.add((ds, DC.license, URIRef("https://creativecommons.org/licenses/by/3.0/")))
+    g.add((ds, DC.modified, Literal("2019-04-02", datatype=XSD.date)))
 
     write_graph(output_base_dir / "foaf", g)
